48d93490747c268b5e619ebc85a46ad6ab7f7714
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2024-07-14 20:25:26 +0100

    api doc

---
+++ README.md
@@ -0,0 +1,15 @@
+# HNG TASK 3
+This is the OPENAPI specifications folder
+
+### Running the server
+To run the server, run:
+
+```
+npm start
+```
+
+To view the Swagger UI interface:
+
+```
+open http://localhost:8080/docs
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs'); // if using a YAML spec
+// const swaggerDocument = require('./api/openapi.json'); // if using a JSON spec
+
+const app = express();
+const port = 5000;
+
+const swaggerDocument = YAML.load('./openapi.yaml'); // if using a YAML spec
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Swagger UI is available on http://localhost:${port}/api-docs`);
+});
